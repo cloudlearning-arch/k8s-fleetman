f73b82c700d17431b688bf65cdecc2e31ce36600
ibrahim-sobhy <ibrahim.sobhy.cse07@gmail.com>
2020-10-05 15:42:34 +0200

    memic truck simulator

--- k8s-fleetman-webapp-angular/src/app/vehicle.service.ts
+++ k8s-fleetman-webapp-angular/src/app/vehicle.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Vehicle } from './vehicle';
-import { Observable ,  Subscription, BehaviorSubject ,  of } from 'rxjs';
+import { Observable, Subscription, ReplaySubject, BehaviorSubject ,  of, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 
@@ -12,31 +12,68 @@ import {  LatLng } from 'leaflet';
 @Injectable()
 export class VehicleService  {
 
-  subscription: BehaviorSubject<Vehicle>;
+  subscription: ReplaySubject<Vehicle>;
   centerVehicle: BehaviorSubject<Vehicle>;
   centerVehicleHistory: BehaviorSubject<any>;
 
   constructor(private _stompService: StompService, private http: HttpClient) {
     // Store local reference to Observable
     // for use with template ( | async )
-    this.subscribe();
-    this.subscription = new BehaviorSubject(null);
+    var messages = new Subject();
+    this.subscribe(messages);
+    this.subscription = new ReplaySubject(5);
     this.centerVehicle = new BehaviorSubject(null);
     this.centerVehicleHistory = new BehaviorSubject(null);
+
+    messages.next(
+      {
+        "body": {
+          "name": "Ibrahims Truck",
+          "lat": 53.35799806751310825347900390625,
+          "longitude": -1.5097549371421337127685546875,
+          "timestamp": "1601899291",
+          "speed": 70
+        }
+      }
+    );
+    messages.next(
+      {
+        "body": {
+          "name": "City Truck",
+          "lat": 53.3798165805637836456298828125,
+          "longitude": -1.46622528322041034698486328125,
+          "timestamp": "1601898306",
+          "speed": 50
+        }
+      }
+    );
+
+    messages.next(
+      {
+        "body": {
+          "name": "City2 Truck",
+          "lat": 53.3798165805637836456298828125,
+          "longitude": -1.46622528322041034698486328125,
+          "timestamp": "1601898306",
+          "speed": 50
+        }
+      }
+    );
   }
 
-  subscribe() {
+  subscribe(messages) {
     // Stream of messages
-    var messages = this._stompService.subscribe('/vehiclepositions/messages');
+    // var messages = this._stompService.subscribe('/vehiclepositions/messages');
+    
+    
     // Subscribe a function to be run on_next message
     messages.subscribe(this.onMessage);
   }
 
   /** Consume a message from the _stompService */
-  onMessage = (message: Message) => {
-
-    let body = JSON.parse(message.body);
-
+  onMessage = (message) => {
+    let body = message.body;
+    console.log('Get message' + message);
     // update vehicle and notify
     let newVehicle = new Vehicle(body.name,
                                  Number(body.lat),
